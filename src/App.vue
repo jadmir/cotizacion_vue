1ba95fa1317df4980edaf545be7f6426dec1e435
jadmir <jadmir1111@gmail.com>
2024-05-02 13:12:00 -0500

    avance

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+import Header from "./components/Header.vue";
+
+const cantidad = ref(10000)
+
+const MIN = 0
+const MAX = 20000
+const STEP = 100
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        v-model.number="cantidad"
+      />
+
+      {{cantidad}}
+    </div>
+  </div>
+</template>
