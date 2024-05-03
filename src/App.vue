67ce7e74fc717a15cf0ba77255ee9278a60cd606
JADMIR IGNACIO VICENTE SIFUENTES <jvicentes@utp.edu.pe>
2024-05-02 21:33:23 -0500

    botones

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 import Header from "./components/Header.vue";
 
 const cantidad = ref(10000)
@@ -8,12 +8,53 @@ const MIN = 0
 const MAX = 20000
 const STEP = 100
 
+const formatearDinero = computed(() => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  })
+
+  return formatter.format(cantidad.value)
+})
+
+const handleChangeDecremento = () => {
+
+  const valor = cantidad.value - STEP
+
+  if(valor < MIN) {
+    alert('cantidad no valida')
+    return
+  }
+  cantidad.value = valor
+} 
+
+const handleChangeIncremento = () => {
+
+  const valor = cantidad.value + STEP
+
+  if(valor > MAX ) {
+    alert('Cantidad no valida')
+    return
+  }
+  cantidad.value = cantidad
+}
 
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="flex justify-between mt-10">
+      <button
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeDecremento"
+      >-</button>
+      <button
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeIncremento"
+      >+</button>
+    </div>
 
     <div class="my-5">
       <input
@@ -25,7 +66,7 @@ const STEP = 100
         v-model.number="cantidad"
       />
 
-      {{cantidad}}
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{formatearDinero}}</p>
     </div>
   </div>
 </template>
